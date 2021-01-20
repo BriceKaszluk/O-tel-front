7d797acdd0e82922b8c19527662261c7f066cc98
MathieuLasserre <mathieu.lasserre2@gmail.com>
2021-01-20 13:19:38 +0100

    components Housing, no data, no style added yet

--- Front/src/components/App/index.js
+++ Front/src/components/App/index.js
@@ -10,7 +10,6 @@ import './styles.css';
 // == Composant
 const App = () => (
   <div className="app">
-    <img src={reactLogo} alt="react logo" />
     <h1>Composant : App</h1>
     <Nav />
   </div>
---
+++ Front/src/components/Housing/index.js
@@ -0,0 +1,35 @@
+import React, { useState } from 'react';
+import Nav from 'src/components/Nav'
+
+
+export default (name, nbPlace, housingPrice, housingDescription) => {
+
+
+    return(
+
+      <div className = "housing">
+        <section className = "hero">
+          <div className = "hero-body">
+            <Nav />
+          </div>
+        </section>
+
+        <div className = "columns">
+          <div className = "column">
+            <h1> *name* </h1>
+            <h2> Logement pour *nbPlace* personnes</h2>
+            <h2> *housingPrice* par nuit</h2>
+            <p> *housingDescription* </p>
+
+          </div>
+          <div className = "column">
+            <h1> *Composant calendrier* </h1>
+            <button className = "is-primary">Réserver</button>
+          </div>
+        </div>
+
+        <h1>*Composant footer*</h1>
+      </div>
+
+    )
+}
