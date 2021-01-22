cdd7032c66aa7b621fe4bf934002d87d6a47ef7b
David Ferrero <ferrero_d@hotmail.com>
2021-01-22 13:25:07 +0100

    config eslint up dated quotes 2

--- Front/src/components/Profil/index.js
+++ Front/src/components/Profil/index.js
@@ -4,26 +4,26 @@ import Nav from 'src/components/Nav';
 import './styles.scss';
 
 export default () => (
-    <container className="profil__container">
-        <title>Profil</title>
-        <Nav />
-        <div className="profil__content">
-            <section className="profil__section1">
-                <div className="profil__main__head">
-                    why not.
-                </div>
-                <div className="profil__main__body">
-
-                    <span className="label">
-                        g
-                    </span>
-
-                </div>
-            </section>
-            <section className="profil__section2">
-                <div>p</div>
-                <div>p</div>
-            </section>
+  <container className="profil__container">
+    <title>Profil</title>
+    <Nav />
+    <div className="profil__content">
+      <section className="profil__section1">
+        <div className="profil__main__head">
+          why not.
         </div>
-    </container>
+        <div className="profil__main__body">
+
+          <span className="label">
+            g
+          </span>
+
+        </div>
+      </section>
+      <section className="profil__section2">
+        <div>p</div>
+        <div>p</div>
+      </section>
+    </div>
+  </container>
 );
