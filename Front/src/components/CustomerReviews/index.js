c53105b76dc99df67b8cb9a3b2a2835c769736cd
teddy-koro <ted.techer@gmail.com>
2021-01-23 05:07:25 +0100

    update components-contact profil-picture

--- Front/src/components/CustomerReviews/index.js
+++ Front/src/components/CustomerReviews/index.js
@@ -1,6 +1,6 @@
 import React from 'react';
 import { Link } from 'react-router-dom';
-
+import picture from 'src/assets/images/profil-picture.jpg';
 import './styles.scss';
 
 const CustomerReviews = () => (
@@ -8,6 +8,9 @@ const CustomerReviews = () => (
   <div className="card">
     <div className="card-content ">
       <div className="media-content">
+        <div className="image">
+          <img src={picture} className="image is-64x64 is-rounded " />
+        </div>
         <p className="title is-4">John Smith</p>
         <p className="subtitle is-6">@johnsmith</p>
       </div>
@@ -16,11 +19,11 @@ const CustomerReviews = () => (
         quicquid increpuisset, ad salutis suae dispendium existimans factum aut cogitatum,
         insontium caedibus fecit victoriam luctuosam."
       </p>
+      <br />
+      <time className="is-centered" dateTime="2021-1">Jan 2021</time>
+
+      <Link to="/livre_d_or" className="button is-primary">Afficher Plus</Link>
     </div>
-    <time dateTime="2021-1">Jan 2021</time>
-
-    <Link to="/livre_d_or" className="button is-primary">Afficher Plus</Link>
-
   </div>
 );
 
